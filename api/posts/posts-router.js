7f4ab9d19dd790ab36ba192f966af089fbbbc28a
Allison Stewart <amstewart@yahoo.com>
2022-02-08 14:42:50 -0500

    added get posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,19 @@
 // implement your posts router here
+const Post = require('./posts-model');
+
+const router = require('express').Router();
+
+///posts endpoints
+router.get('/', (req, res) => {
+  Post.find(req.query)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: 'The posts information could not be retrieved'})
+    })
+})
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -4,7 +4,11 @@
 const express = require('express');
 const server = express();
 
+const postsRouter = require('./posts/posts-router');
+
 server.use(express.json());
+
+server.use('/api/posts', postsRouter);
 
 server.use('*', (req, res) => {
   res.status(404).send(`
