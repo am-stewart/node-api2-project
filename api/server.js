808bfc9e1fbf2dff763c419ed602fdf709f7e31d
Allison Stewart <amstewart@yahoo.com>
2022-02-08 14:32:36 -0500

    set up server and running on port 9000

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
 // require your posts router and connect it here
+
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.use('*', (req, res) => {
+  res.status(404).send(`
+    <p> Oops, we cannot find that resource</p>
+  `)
+})
+
+module.exports = server;
